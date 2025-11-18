ae4d23cf9dfc6ab78d4889dd2d124dc2366cf81b
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2025-11-18 07:14:20 +0100

    Fix: Handle ConnectionTimeout exception in Discord RPC connection

--- modules/discord/rpc.py
+++ modules/discord/rpc.py
@@ -89,7 +89,7 @@ def _run(rpc: Presence, queue: QueueType, connection_status: Event) -> None:
         if not connection_status.is_set():
             try:
                 rpc.connect()
-            except (exceptions.DiscordNotFound, exceptions.DiscordError):
+            except (exceptions.DiscordNotFound, exceptions.DiscordError, exceptions.ConnectionTimeout):
                 continue
             else:
                 connection_status.set()
