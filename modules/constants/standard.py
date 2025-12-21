f27ca5db22aca1cf45e1269bb01ec5773d5e679c
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2025-12-21 18:43:07 +0100

    Refactor SYSTEMROOT_PATH to use os.getenv
    Replaces os.environ.get with os.getenv for retrieving the SYSTEMROOT environment variable, improving code readability and consistency.

--- modules/constants/standard.py
+++ modules/constants/standard.py
@@ -2,7 +2,7 @@
 import os
 from pathlib import Path
 
-SYSTEMROOT_PATH = Path(os.environ.get('SYSTEMROOT', 'C:/Windows'))  # Get the SystemRoot environment variable dynamically
+SYSTEMROOT_PATH = Path(os.getenv('SYSTEMROOT', 'C:/Windows'))
 SYSTEM32_PATH = SYSTEMROOT_PATH / 'System32'
 CMD_EXE = SYSTEM32_PATH / 'cmd.exe'
 SC_EXE = SYSTEM32_PATH / 'sc.exe'
