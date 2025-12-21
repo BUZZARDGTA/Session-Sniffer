1881b166512bae58ae86170629f8e48f6e09dadb
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2025-12-21 18:06:15 +0100

    Clarify documentation for constants package
    Reworded the docstring in modules/constants/__init__.py for improved clarity on when to include constants in the package.

--- modules/constants/__init__.py
+++ modules/constants/__init__.py
@@ -1,6 +1,7 @@
 """This package contains constants used throughout the project.
 
-IMPORTANT: Constants in this folder should be used in at least 2 files to justify
-their presence here. If a constant is only used in one file, it should be defined
-directly in that file instead of being placed in this constants package.
+Note:
+    A constant should appear in at least two files to justify inclusion here.
+    If a constant is used in only one file, define it directly in that file
+    instead of placing it in this package.
 """
