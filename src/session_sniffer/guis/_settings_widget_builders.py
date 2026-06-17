e00e8f03cf8b25d62e7e8de22b40e58ca90c7bcf
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2026-06-17 10:47:42 +0200

    feat: add MICROSOFT range block to Call of Duty: WWII preset

--- src/session_sniffer/guis/_settings_widget_builders.py
+++ src/session_sniffer/guis/_settings_widget_builders.py
@@ -366,6 +366,7 @@ def create_third_party_servers_split_widget(key: str, meta: SettingMeta) -> QWid
             'DEMONWARE',
             'FRIEND_IT',
             'LATITUDE_SH',
+            'MICROSOFT',
             'TENCENT',
             'THE_CONSTANT_COMPANY',
             'TSEFLOW',
