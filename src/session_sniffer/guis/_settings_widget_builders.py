8d4bad29b6b5814432096fd8b8ef4b6beb2df464
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2026-07-05 20:59:44 +0200

    feat: add Dying Light support and filter Amazon/Google IPs

--- src/session_sniffer/guis/_settings_widget_builders.py
+++ src/session_sniffer/guis/_settings_widget_builders.py
@@ -263,6 +263,7 @@ def create_third_party_servers_split_widget(key: str, meta: SettingMeta) -> QWid
         'Call of Duty®: WWII',
         'Chatspin',
         'Discord',
+        'Dying Light',
         'Grand Theft Auto Online',
         "John Carpenter's Toxic Commando",
         'Minecraft Bedrock Edition',
@@ -449,6 +450,10 @@ def create_third_party_servers_split_widget(key: str, meta: SettingMeta) -> QWid
         'Discord': {
             'CLOUDFLARE',
             'DISCORD',
+        },
+        'Dying Light': {
+            'AMAZON',
+            'GOOGLE_LLC',
         },
         'Grand Theft Auto Online': {
             'BATTLEYE',
