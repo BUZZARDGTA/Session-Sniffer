53c2ae6a5828ff0822010e304b5115f32c1235b3
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2026-07-06 21:24:42 +0200

    Add US_DEPARTMENT_OF_DEFENSE blocking ranges to Dying Light

--- src/session_sniffer/guis/_settings_widget_builders.py
+++ src/session_sniffer/guis/_settings_widget_builders.py
@@ -475,6 +475,7 @@ def create_third_party_servers_split_widget(key: str, meta: SettingMeta) -> QWid
         'Dying Light': {
             'AMAZON',
             'GOOGLE_LLC',
+            'US_DEPARTMENT_OF_DEFENSE',
         },
         'Dying Light 2': {
             'AMAZON',
