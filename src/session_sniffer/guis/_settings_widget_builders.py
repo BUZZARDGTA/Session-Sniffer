448a2e94657722579c4afe92fbbfea9882406110
IB_U_Z_Z_A_R_Dl <62464560+BUZZARDGTA@users.noreply.github.com>
2026-07-06 21:11:52 +0200

    Update Breakpoint configuration and notes

--- src/session_sniffer/guis/_settings_widget_builders.py
+++ src/session_sniffer/guis/_settings_widget_builders.py
@@ -560,6 +560,7 @@ def create_third_party_servers_split_widget(key: str, meta: SettingMeta) -> QWid
             'GOOGLE_LLC',
         },
         "Tom Clancy's Ghost Recon® Breakpoint": {
+            'AMAZON',
             'I3D_NET',
         },
         'UNO': {
